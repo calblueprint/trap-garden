923fbae4d0986aee5639699406404adc85532a24
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-09-30 14:09:35 -0700

    run prettier

--- api/supabase/queries/plantSeasonality.ts
+++ api/supabase/queries/plantSeasonality.ts
@@ -4,9 +4,10 @@ import supabase from '../createClient';
 export async function getPlantSeasonality(
   input_state: string,
 ): Promise<Plant[]> {
-  const { data, error } = await supabase.rpc('get_plant_seasonality', {
-    input_state,
-  });
+  const { data, error } = await supabase
+    .from('plant_seasonality')
+    .select('*')
+    .eq('state', input_state);
   if (error)
     throw new Error(`Error fetching plant seasonality: ${error.message}`);
   return data;
