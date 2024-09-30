5df80896e255f63b05e99403e998e67aabbba41d
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-09-29 18:45:48 -0700

    created PlantList type and getPlantSeasonality query

--- api/supabase/queries/plantSeasonality.ts
+++ api/supabase/queries/plantSeasonality.ts
@@ -1,8 +1,12 @@
 import { Plant } from '@/types/schema';
 import supabase from '../createClient';
 
-export async function getPlantSeasonality(): Promise<Plant[]> {
-  const { data, error } = await supabase.rpc('get_plant_seasonality');
+export async function getPlantSeasonality(
+  input_state: string,
+): Promise<Plant[]> {
+  const { data, error } = await supabase.rpc('get_plant_seasonality', {
+    input_state,
+  });
   if (error)
     throw new Error(`Error fetching plant seasonality: ${error.message}`);
   return data;
---
+++ components/PlantList.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import { getPlantSeasonality } from '@/api/supabase/queries/plantSeasonality';
+import { Plant } from '@/types/schema';
+
+export const PlantList = () => {
+  const [plants, setPlants] = useState<Plant[]>([]);
+
+  useEffect(() => {
+    const fetchPlantSeasonality = async () => {
+      const plantList = await getPlantSeasonality('Tennessee');
+      setPlants(plantList);
+    };
+
+    fetchPlantSeasonality();
+  }, []);
+
+  return (
+    <div>
+      {plants.map((plant, key) => (
+        <div key={key}>{plant.plant_name}</div>
+      ))}
+    </div>
+  );
+};
