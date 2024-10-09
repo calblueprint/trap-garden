d387c9030e79bcbdce9638145ed3c557e541dee9
rachaelch3n <rchen2027@berkeley.edu>
2024-10-08 17:59:00 -0700

    added placeholders and redirects to home page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 import supabase from '../../../api/supabase/createClient';
 
 export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { push } = useRouter();
+
   const handleSignUp = async () => {
     const { data, error } = await supabase.auth.signUp({
       email,
@@ -13,13 +16,24 @@ export default function Login() {
     });
 
     if (error) {
-      throw new Error(`An error occurred trying to sign up: ${error}`);
+      // Check if the error is due to an already registered email
+      if (error.message.includes('User already registered')) {
+        throw new Error(
+          'This email is already registered. Please try signing in instead.',
+        );
+      } else {
+        throw new Error(
+          `An error occurred trying to sign up: ${error.message}`,
+        );
+      }
     }
+
+    push('/');
 
     return data;
   };
 
-  const signInWithEmail = async () => {
+  const handleSignInWithEmail = async () => {
     const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password,
@@ -28,6 +42,8 @@ export default function Login() {
     if (error) {
       throw new Error(`An error occurred trying to sign in: ${error}`);
     }
+
+    push('/');
 
     return data;
   };
@@ -38,17 +54,19 @@ export default function Login() {
         name="email"
         onChange={e => setEmail(e.target.value)}
         value={email}
+        placeholder="Email"
       />
       <input
         type="password"
         name="password"
         onChange={e => setPassword(e.target.value)}
         value={password}
+        placeholder="Password"
       />
       <button type="button" onClick={handleSignUp}>
         Sign up
       </button>
-      <button type="button" onClick={signInWithEmail}>
+      <button type="button" onClick={handleSignInWithEmail}>
         Sign in
       </button>
     </>
