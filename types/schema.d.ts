be7cb70ab27791da88ab749488ff76f1d28ce330
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-10-15 21:34:56 -0700

    schema changes, adding water_frequency, weed_frequency, sunlight, difficulty_level

--- types/schema.d.ts
+++ types/schema.d.ts
@@ -1,13 +1,16 @@
 import type { UUID } from 'crypto';
 
-export type Season = 'SPRING' | 'SUMMER' | 'FALL' | 'WINTER';
+export type SeasonEnum = 'SPRING' | 'SUMMER' | 'FALL' | 'WINTER';
+
+export type DifficultyLevelEnum = 'EASY' | 'MODERATE' | 'HARD';
 
 export interface Plant {
   id: UUID;
   plant_name: string;
   state: string;
-  harvest_season: Season;
-  water_num_times_per_week: number;
+  harvest_season: SeasonEnum;
+  water_frequency: string;
+  weed_frequency: string;
   plant_seed_indoors_start: string;
   plant_seed_indoors_end: string;
   plant_seed_outdoors_start: string;
@@ -16,10 +19,10 @@ export interface Plant {
   plant_transplant_end: string;
   harvest_start: string;
   harvest_end: string;
-  water_inches_per_week: number;
-  harvest_days_after_plant: number;
-  sunlight_required: string;
   beginner_friendly: boolean;
   plant_tips: string;
   img: string;
+  difficulty_level: DifficultyLevelEnum;
+  sunlight_min_hours: int;
+  sunlight_max_hours: int;
 }
