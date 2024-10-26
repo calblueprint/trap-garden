ac7a8751017d0bc02abbd6c32733ef5f3216cd6e
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-10-26 15:27:43 -0700

    change dropdowns to multi select

--- components/FilterDropdown.tsx
+++ components/FilterDropdown.tsx
@@ -1,51 +1,45 @@
 import React, { useState } from 'react';
+import { MultiSelect } from 'react-multi-select-component';
+import { DropdownOption } from '@/types/schema';
 
 interface FilterDropdownProps {
-  name: string;
-  id: string;
-  value: string;
-  setStateAction: React.Dispatch<React.SetStateAction<string>>;
+  value: DropdownOption[];
+  setStateAction: React.Dispatch<React.SetStateAction<DropdownOption[]>>;
   options: string[];
   placeholder: string;
 }
 
 export default function FilterDropdown({
-  name,
-  id,
   value,
   setStateAction,
   options,
   placeholder,
 }: FilterDropdownProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setStateAction(event.target.value);
-    setIsOpen(false);
-  };
-
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <select
-      name={name}
-      id={id}
-      onChange={handleChange}
-      onClick={handleToggle}
-      onBlur={() => setIsOpen(false)}
+    <MultiSelect
+      options={options.map(option => ({ label: option, value: option }))}
       value={value}
-    >
-      {/*Default placeholder text*/}
-      <option value="" disabled hidden>
-        {placeholder}
-      </option>
-      {options.map((option, index) => (
-        <option key={index} value={option}>
-          {option}
-        </option>
-      ))}
-    </select>
+      onChange={setStateAction}
+      labelledBy={placeholder}
+      hasSelectAll={false}
+    />
+    // <select
+    //   name={name}
+    //   id={id}
+    //   onChange={handleChange}
+    //   onClick={handleToggle}
+    //   onBlur={() => setIsOpen(false)}
+    //   value={value}
+    // >
+    //   {/*Default placeholder text*/}
+    //   <option value="" disabled hidden>
+    //     {placeholder}
+    //   </option>
+    //   {options.map((option, index) => (
+    //     <option key={index} value={option}>
+    //       {option}
+    //     </option>
+    //   ))}
+    // </select>
   );
 }
