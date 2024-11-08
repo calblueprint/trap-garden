1beb0f8b3e346058c2afa882e80443640463e891
Catherine Tan <catherine95118@gmail.com>
2024-11-08 13:53:43 -0800

    prettier fix

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import { AuthProvider, useAuth } from '../../utils/AuthProvider';
+import { useAuth } from '../../utils/AuthProvider';
 
 export default function SignUp() {
   const { signUp } = useAuth();
