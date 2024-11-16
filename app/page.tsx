e90945702f6dacbe06ec38b4edf4c965108b7e46
Catherine Tan <catherine95118@gmail.com>
2024-11-15 20:42:40 -0800

    revert homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import BPLogo from '@/assets/images/bp-logo.png';
-import SingleSelectRadioGroup from '@/components/RadioGroup';
 import { Container, Image } from './page.style';
 
 export default function Home() {
@@ -9,15 +6,6 @@ export default function Home() {
     <Container>
       <Image src={BPLogo} alt="Blueprint Logo" />
       <p>Open up app/page.tsx to get started!</p>
-      <SingleSelectRadioGroup
-        name="exampleRadioGroup"
-        options={[
-          { label: 'School', value: 'option1' },
-          { label: 'Community', value: 'option2' },
-          { label: 'Individual', value: 'option3' },
-        ]}
-        defaultValue="option2"
-      />
     </Container>
   );
 }
