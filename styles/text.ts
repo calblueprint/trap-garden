f5a2b783407cbc962758f1f60355ef0fd6871f02
Catherine Tan <64336644+ccatherinetan@users.noreply.github.com>
2024-11-17 12:17:12 -0800

    [style] create global styles (#45)

---
+++ styles/text.ts
@@ -0,0 +1,34 @@
+import styled, { css } from 'styled-components';
+import COLORS from './colors';
+
+// 1 rem = 16 px
+interface TextProps {
+  $color?: string;
+  $align?: 'left' | 'right' | 'center' | 'end' | 'justify' | 'start';
+}
+
+const TextStyles = css<TextProps>`
+  color: ${({ $color }) => $color || COLORS.black};
+  text-align: ${({ $align }) => $align};
+  margin: 0;
+`;
+
+export const H1 = styled.h1<TextProps>`
+  ${TextStyles}
+  font-size: 2rem; // this may change later
+`;
+
+export const H2 = styled.h2<TextProps>`
+  ${TextStyles}
+  font-size: 1.75rem;
+`;
+
+export const H3 = styled.h3<TextProps>`
+  ${TextStyles}
+  font-size: 1.25rem;
+`;
+
+export const H4 = styled.h4<TextProps>`
+  ${TextStyles}
+  font-size: 1rem;
+`;
