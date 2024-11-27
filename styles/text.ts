89dba4632ec6935ff1dbf14f1a084557ffe9880c
Catherine Tan <catherine95118@gmail.com>
2024-11-26 22:08:57 -0800

    [chore] update global text styles + add fontWeight prop

--- styles/text.ts
+++ styles/text.ts
@@ -4,6 +4,7 @@ import COLORS from './colors';
 // 1 rem = 16 px
 interface TextProps {
   $color?: string;
+  $fontWeight?: number | string;
   $align?: 'left' | 'right' | 'center' | 'end' | 'justify' | 'start';
 }
 
@@ -15,20 +16,42 @@ const TextStyles = css<TextProps>`
 
 export const H1 = styled.h1<TextProps>`
   ${TextStyles}
-  font-size: 2rem; // this may change later
+  font-weight: ${({ $fontWeight }) => $fontWeight || '500'};
+  font-size: 2rem;
 `;
 
 export const H2 = styled.h2<TextProps>`
   ${TextStyles}
+  font-weight: ${({ $fontWeight }) => $fontWeight || '400'};
   font-size: 1.75rem;
 `;
 
 export const H3 = styled.h3<TextProps>`
   ${TextStyles}
-  font-size: 1.25rem;
+  font-weight: ${({ $fontWeight }) => $fontWeight || '400'};
+  font-size: 1.5rem;
 `;
 
 export const H4 = styled.h4<TextProps>`
   ${TextStyles}
+  font-weight: ${({ $fontWeight }) => $fontWeight || '300'};
+  font-size: 1.25rem;
+`;
+
+export const P1 = styled.p<TextProps>`
+  ${TextStyles}
+  font-weight: ${({ $fontWeight }) => $fontWeight || '300'};
   font-size: 1rem;
+`;
+
+export const P2 = styled.p<TextProps>`
+  ${TextStyles}
+  font-weight: ${({ $fontWeight }) => $fontWeight || '300'};
+  font-size: 0.875rem;
+`;
+
+export const P3 = styled.p<TextProps>`
+  ${TextStyles}
+  font-weight: ${({ $fontWeight }) => $fontWeight || '300'};
+  font-size: 0.75rem;
 `;
