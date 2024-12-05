2048963d2fd044fbef8df7706421556be343dcd4
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-12-05 14:06:02 -0800

    apply styling to react-select

--- components/FilterDropdownMultiple/styles.ts
+++ components/FilterDropdownMultiple/styles.ts
@@ -1,40 +1,66 @@
-import { MultiSelect } from 'react-multi-select-component';
+import { StylesConfig } from 'react-select';
 import styled from 'styled-components';
 import COLORS from '@/styles/colors';
+import { DropdownOption } from '@/types/schema';
 
-export const StyledMultiSelect = styled(MultiSelect)`
-  .dropdown-container {
-    border-radius: 60px !important;
-    padding: 0px 14px 0px 14px !important;
-    align-items: center;
-    justify-content: center;
-    justify-items: center;
-    gap: 2px;
-    background-color: #fff !important;
-    border: 0.5px solid ${COLORS.midgray} !important;
-    color: #fff;
-    position: relative;
-  }
-
-  .dropdown-content {
-    display: block !important;
-    position: absolute !important;
-    z-index: 10000 !important;
-    top: 100%;
-    color: ${COLORS.midgray};
-  }
-
-  .dropdown-heading {
-    font-size: 0.75rem !important;
-    padding: 0px !important;
-  }
-
-  .dropdown-heading-value {
-    color: ${COLORS.midgray} !important;
-  }
-
-  // changing color of the default display text
-  .gray {
-    color: ${COLORS.midgray} !important;
-  }
+export const StyledOption = styled.div`
+  display: flex;
+  align-items: center;
 `;
+
+// custom styles for react-select component
+export const customSelectStyles: StylesConfig<DropdownOption<any>, true> = {
+  control: (baseStyles, state) => ({
+    ...baseStyles,
+    borderRadius: '57px',
+    border: `0.5px solid ${COLORS.midgray}`,
+    backgroundColor: state.isDisabled ? COLORS.lightgray : '#fff',
+    padding: '8px 14px',
+    color: COLORS.midgray,
+  }),
+  placeholder: baseStyles => ({
+    ...baseStyles,
+    color: COLORS.midgray,
+    fontSize: '0.75rem',
+    padding: '0px',
+    margin: '0px',
+  }),
+  input: baseStyles => ({
+    ...baseStyles,
+    margin: '0px',
+    padding: '0px',
+  }),
+  // hide vertical bar between arrow and text
+  indicatorSeparator: baseStyles => ({
+    ...baseStyles,
+    display: 'none',
+  }),
+  clearIndicator: baseStyles => ({
+    ...baseStyles,
+    padding: '0px',
+  }),
+  dropdownIndicator: baseStyles => ({
+    ...baseStyles,
+    padding: '0px',
+    marginLeft: '-4px', // move the dropdown indicator to the left cant override text styles
+    color: COLORS.midgray,
+  }),
+  multiValue: baseStyles => ({
+    ...baseStyles,
+    backgroundColor: '#fff',
+    border: '0px',
+    padding: '0px',
+    margin: '0px',
+  }),
+  multiValueLabel: baseStyles => ({
+    ...baseStyles,
+    fontSize: '0.75rem',
+    color: `${COLORS.midgray} !important`,
+    padding: '0px',
+    paddingLeft: '0px',
+  }),
+  multiValueRemove: baseStyles => ({
+    ...baseStyles,
+    display: 'none',
+  }),
+};
