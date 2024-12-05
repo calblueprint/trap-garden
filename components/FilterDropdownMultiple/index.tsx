87a4d37ec54ca3ad9adaacd3481feb18d63ff38b
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2024-12-05 14:06:02 -0800

    apply styling to react-select

--- components/FilterDropdownMultiple/index.tsx
+++ components/FilterDropdownMultiple/index.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import Select, { MultiValue } from 'react-select';
+import React, { useState } from 'react';
+import Select, {
+  ActionMeta,
+  components,
+  GroupBase,
+  MultiValue,
+  MultiValueProps,
+  OptionProps,
+} from 'react-select';
+import { P3 } from '@/styles/text';
 import { DropdownOption } from '@/types/schema';
-import { StyledMultiSelect } from './styles';
+import { customSelectStyles, StyledOption } from './styles';
 
 interface FilterDropdownProps<T> {
   value: DropdownOption<T>[];
@@ -18,8 +26,75 @@ export default function FilterDropdownMultiple<T>({
   placeholder,
   disabled = false,
 }: FilterDropdownProps<T>) {
-  const handleChange = (selectedOptions: MultiValue<DropdownOption<T>>) => {
+  const [userSelectedOptions, setUserSelectedOptions] = useState<
+    DropdownOption<T>[]
+  >([]);
+
+  const handleChange = (
+    selectedOptions: MultiValue<DropdownOption<T>>,
+    _actionMeta: ActionMeta<DropdownOption<T>>,
+  ) => {
     setStateAction(selectedOptions as DropdownOption<T>[]);
+    setUserSelectedOptions(selectedOptions as DropdownOption<T>[]);
+  };
+
+  // overrides the default MultiValue to display custom text
+  // displays first selected value followed by + n if more than 1 selected
+  // StyledMultiValue appears for each selected option, so if more than 1 is selected,
+  // the rest of the selected options are not shown, instead the + n is shown as part of the first option
+  const StyledMultiValue = ({
+    children,
+    ...props
+  }: MultiValueProps<
+    DropdownOption<T>,
+    true,
+    GroupBase<DropdownOption<T>>
+  >) => {
+    const { selectProps, data } = props;
+    if (Array.isArray(selectProps.value)) {
+      // find index of the selected option and check if its the first
+      const index = selectProps.value.findIndex(
+        (option: DropdownOption<T>) => option.value === data.value,
+      );
+      const isFirst = index === 0;
+      // find number of remaining selected options
+      const additionalCount = selectProps.value.length - 1;
+
+      return (
+        <P3>
+          {/* display label of first selected option */}
+          {isFirst ? (
+            <>
+              {data.label}
+              {/* display additional count only if more than one option is selected*/}
+              {additionalCount > 0 && ` +${additionalCount}`}
+            </>
+          ) : // don't display anything if not the first selected option
+          null}
+        </P3>
+      );
+    }
+
+    // nothing is selected yet
+    return null;
+  };
+
+  // overrides the default Options to display a checkbox that ticks when option selected
+  const CustomOption = (
+    props: OptionProps<DropdownOption<T>, true, GroupBase<DropdownOption<T>>>,
+  ) => {
+    return (
+      <components.Option {...props}>
+        <StyledOption>
+          <input
+            type="checkbox"
+            checked={props.isSelected}
+            style={{ marginRight: 8 }} // spacing between checkbox and text
+          />
+          {props.label}
+        </StyledOption>
+      </components.Option>
+    );
   };
 
   return (
@@ -31,6 +106,11 @@ export default function FilterDropdownMultiple<T>({
       placeholder={placeholder}
       onChange={handleChange}
       closeMenuOnSelect={false}
+      styles={customSelectStyles}
+      isSearchable={false}
+      hideSelectedOptions={false}
+      isClearable={false}
+      components={{ MultiValue: StyledMultiValue, Option: CustomOption }}
     />
   );
 }
