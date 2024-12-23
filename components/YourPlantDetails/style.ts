d86754d0b6fbaf8c42e33363f7d141199b1d9be4
Catherine Tan <catherine95118@gmail.com>
2024-12-23 15:50:04 -0800

    replace editbutton with SmallButton in YourPlantDetails

--- components/YourPlantDetails/style.ts
+++ components/YourPlantDetails/style.ts
@@ -1,6 +1,5 @@
 import styled from 'styled-components';
 import COLORS from '@/styles/colors';
-import { SmallRoundedButton } from '../Button';
 
 export const Container = styled.div`
   padding: 16px;
@@ -13,13 +12,4 @@ export const Header = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-bottom: 16px;
-`;
-
-// Not Used Yet
-export const EditButton = styled(SmallRoundedButton)`
-  font-size: 0.875rem;
-  padding: 0.25rem 0.5rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
 `;
