9e9d037881e6522ffadfb89a02666a93100b49a8
rachaelch3n <rchen2027@berkeley.edu>
2025-02-24 02:10:25 -0800

    reset email password sends now

--- app/(auth)/email-sent/page.tsx
+++ app/(auth)/email-sent/page.tsx
@@ -1,14 +1,3 @@
-import { useRouter } from 'next/navigation';
-import { BackButton } from '@/app/plant-page/style';
-
-const router = useRouter();
-
-<BackButton
-  type="button"
-  onClick={e => {
-    e.preventDefault(); // prevents accidental form submission
-    router.push('/login');
-  }}
->
-  ←
-</BackButton>;
+export default function EmailSent() {
+  return <div>email</div>;
+}
