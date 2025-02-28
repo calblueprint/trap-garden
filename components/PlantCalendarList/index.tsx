657087450a748bf449cfec0ab09233ffe99cf1a9
Torin Nayak <torinnayak@berkeley.edu>
2025-02-27 22:43:22 -0800

    fr this time

--- components/PlantCalendarList/index.tsx
+++ components/PlantCalendarList/index.tsx
@@ -26,7 +26,6 @@ interface PlantListProps {
   growingSeasonFilterValue: DropdownOption<SeasonEnum>[];
   usStateFilterValue: DropdownOption<string> | null;
   searchTerm: string;
-  isSelected?: boolean;
   onClick?: () => void;
 }
 
