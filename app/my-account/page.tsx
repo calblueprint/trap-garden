b5542e28d4d0b793d1621a549aa104d31dae8d11
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2025-03-12 16:12:53 -0700

    adjust flex layout of info containers for log out button spacing

--- app/my-account/page.tsx
+++ app/my-account/page.tsx
@@ -40,186 +40,182 @@ export default function MyAccount() {
   }, [userEmail, loading, router]);
 
   return (
-    <Flex $direction="column">
-      {/* User profile image and name */}
-      <ProfilePictureContainer>
-        <div
-          style={{
-            display: 'flex',
-            paddingLeft: '1.5rem',
-            width: '100%',
-          }}
-        >
-          <BackButton
-            onClick={() => {
-              router.back();
-            }}
-          >
-            ←
-          </BackButton>
-        </div>
-
-        <ProfileIcon type="profile" />
-        <H4
-          $color={COLORS.shrub}
-          style={{
-            textAlign: 'center',
-            marginTop: '1.5rem',
-          }}
-        >
-          My Account
-        </H4>
-      </ProfilePictureContainer>
-
-      {/* Information regarding user personal information */}
-      <PersonalInformationContainer>
-        <P1
-          $color={COLORS.shrub}
-          style={{
-            marginBottom: '1rem',
-            marginTop: '1.5rem',
-          }}
-        >
-          Personal Information
-        </P1>
-        <InfoField>
-          <P2 $fontWeight={400} $color={COLORS.darkgray}>
-            Name
-          </P2>
-          <P2
-            style={{
-              marginBottom: '1.5rem',
-            }}
-            $fontWeight={300}
-            $color={COLORS.darkgray}
-          >
-            Kyrene Tam
-          </P2>
-        </InfoField>
-
-        <InfoField>
-          <P2 $fontWeight={400} $color={COLORS.darkgray}>
-            Email
-          </P2>
-          <P2
-            style={{
-              marginBottom: '2.5rem',
-            }}
-            $fontWeight={300}
-            $color={COLORS.darkgray}
-          >
-            {userEmail}
-          </P2>
-        </InfoField>
-      </PersonalInformationContainer>
-
-      {/* Information regarding user's garden information */}
-      <GardenInformationContainer>
-        <P1
-          $color={COLORS.shrub}
-          style={{
-            marginBottom: '1rem',
-          }}
-        >
-          Garden Information
-        </P1>
-
-        {/* Displays onboarding button if user profile is not ready */}
-        {profileReady && !profileData ? (
+    <Flex $direction="column" $minH="calc(100vh - 60px)" $justify="between">
+      <div>
+        {/* User profile image and name */}
+        <ProfilePictureContainer>
           <div
             style={{
               display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-              justifyContent: 'center',
+              paddingLeft: '1.5rem',
               width: '100%',
-              height: '100%',
             }}
           >
-            <BigButton
-              $primaryColor={COLORS.shrub}
-              onClick={() => router.push('/onboarding')}
-              style={{
-                width: '100%',
+            <BackButton
+              onClick={() => {
+                router.back();
               }}
             >
-              Go To Onboarding
-            </BigButton>
-
-            <P3
-              $fontWeight={300}
-              $color={COLORS.errorRed}
-              style={{
-                marginTop: '0.6875rem',
-                textAlign: 'center',
-              }}
-            >
-              Profile Information Incomplete
-            </P3>
+              ←
+            </BackButton>
           </div>
-        ) : (
-          <>
-            {/* Displays garden information if user profile is ready */}
-            <InfoField>
-              <P2 $fontWeight={400} $color={COLORS.darkgray}>
-                Location
-              </P2>
-              <P2
-                style={{
-                  marginBottom: '1.5rem',
-                }}
-                $fontWeight={300}
-                $color={COLORS.darkgray}
-              >
-                {userId && profileReady && toTitleCase(profileData!.us_state)}
-              </P2>
-            </InfoField>
 
-            <InfoField>
-              <P2 $fontWeight={400} $color={COLORS.darkgray}>
-                Garden Type
-              </P2>
-              <P2
-                style={{
-                  marginBottom: '1.5rem',
-                }}
-                $fontWeight={300}
-                $color={COLORS.darkgray}
-              >
-                {profileReady &&
-                  userTypeLabels[profileData?.user_type as UserTypeEnum]}
-              </P2>
-            </InfoField>
+          <ProfileIcon type="profile" />
+          <H4
+            $color={COLORS.shrub}
+            style={{
+              textAlign: 'center',
+              marginTop: '1.5rem',
+            }}
+          >
+            My Account
+          </H4>
+        </ProfilePictureContainer>
 
-            <InfoField>
-              <P2 $fontWeight={400} $color={COLORS.darkgray}>
-                Plot Status
-              </P2>
-              <P2 $fontWeight={300} $color={COLORS.darkgray}>
-                {
-                  plotOptions.find(
-                    option => option.value === profileData?.has_plot,
-                  )?.label
-                }
-              </P2>
-            </InfoField>
-          </>
-        )}
-      </GardenInformationContainer>
+        {/* Information regarding user personal information */}
+        <PersonalInformationContainer>
+          <P1
+            $color={COLORS.shrub}
+            style={{
+              marginBottom: '1rem',
+              marginTop: '1.5rem',
+            }}
+          >
+            Personal Information
+          </P1>
+          <InfoField>
+            <P2 $fontWeight={400} $color={COLORS.darkgray}>
+              Name
+            </P2>
+            <P2
+              style={{
+                marginBottom: '1.5rem',
+              }}
+              $fontWeight={300}
+              $color={COLORS.darkgray}
+            >
+              Kyrene Tam
+            </P2>
+          </InfoField>
+
+          <InfoField>
+            <P2 $fontWeight={400} $color={COLORS.darkgray}>
+              Email
+            </P2>
+            <P2
+              style={{
+                marginBottom: '2.5rem',
+              }}
+              $fontWeight={300}
+              $color={COLORS.darkgray}
+            >
+              {userEmail}
+            </P2>
+          </InfoField>
+        </PersonalInformationContainer>
+
+        {/* Information regarding user's garden information */}
+        <GardenInformationContainer>
+          <P1
+            $color={COLORS.shrub}
+            style={{
+              marginBottom: '1rem',
+            }}
+          >
+            Garden Information
+          </P1>
+
+          {/* Displays onboarding button if user profile is not ready */}
+          {profileReady && !profileData ? (
+            <div
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '100%',
+                height: '100%',
+              }}
+            >
+              <BigButton
+                $primaryColor={COLORS.shrub}
+                onClick={() => router.push('/onboarding')}
+                style={{
+                  width: '100%',
+                }}
+              >
+                Go To Onboarding
+              </BigButton>
+
+              <P3
+                $fontWeight={300}
+                $color={COLORS.errorRed}
+                style={{
+                  marginTop: '0.6875rem',
+                  textAlign: 'center',
+                }}
+              >
+                Profile Information Incomplete
+              </P3>
+            </div>
+          ) : (
+            <>
+              {/* Displays garden information if user profile is ready */}
+              <InfoField>
+                <P2 $fontWeight={400} $color={COLORS.darkgray}>
+                  Location
+                </P2>
+                <P2
+                  style={{
+                    marginBottom: '1.5rem',
+                  }}
+                  $fontWeight={300}
+                  $color={COLORS.darkgray}
+                >
+                  {userId && profileReady && toTitleCase(profileData!.us_state)}
+                </P2>
+              </InfoField>
+
+              <InfoField>
+                <P2 $fontWeight={400} $color={COLORS.darkgray}>
+                  Garden Type
+                </P2>
+                <P2
+                  style={{
+                    marginBottom: '1.5rem',
+                  }}
+                  $fontWeight={300}
+                  $color={COLORS.darkgray}
+                >
+                  {profileReady &&
+                    userTypeLabels[profileData?.user_type as UserTypeEnum]}
+                </P2>
+              </InfoField>
+
+              <InfoField>
+                <P2 $fontWeight={400} $color={COLORS.darkgray}>
+                  Plot Status
+                </P2>
+                <P2 $fontWeight={300} $color={COLORS.darkgray}>
+                  {
+                    plotOptions.find(
+                      option => option.value === profileData?.has_plot,
+                    )?.label
+                  }
+                </P2>
+              </InfoField>
+            </>
+          )}
+        </GardenInformationContainer>
+      </div>
 
       {/* Log out button*/}
       <div
         style={{
-          padding: '0 1.5rem',
+          padding: '0 1.5rem 3.5rem 1.5rem',
         }}
       >
-        <BigButton
-          $secondaryColor={COLORS.errorRed}
-          onClick={handleSignOut}
-          style={{
-            marginTop: profileReady && !profileData ? '9.0625rem' : '45px',
-          }}
-        >
+        <BigButton $secondaryColor={COLORS.errorRed} onClick={handleSignOut}>
           Log Out
         </BigButton>
       </div>
