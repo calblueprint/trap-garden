e969204c8b3250eb72e76ccc929c2088634307de
Torin Nayak <torinnayak@berkeley.edu>
2025-03-11 18:41:49 -0700

    Added "enter" functionaliy for submit buttons (onboarding, add-details, login, signup) and "esc" for the nav bar

--- components/NavColumn/index.tsx
+++ components/NavColumn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation';
 import CONFIG from '@/lib/configs';
@@ -160,6 +160,21 @@ export default function NavColumn({ isOpen, onClose }: NavColumnProps) {
       </LoginButtonsContainer>
     );
   };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const key = e.key;
+      if (key === 'Escape') {
+        onClose();
+      }
+    };
+
+    //add listener for keydown events
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
   return (
     <>
