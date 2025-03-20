fc7a2c8182f73648faa00b83aa592ebbf8696b7f
kaleenchen <kaleenchen@berkeley.edu>
2025-03-20 16:04:38 -0700

    made minor changes during work sess

--- components/CustomSelect/styles.ts
+++ components/CustomSelect/styles.ts
@@ -1,3 +1,57 @@
+// import styled from 'styled-components';
+// import COLORS from '@/styles/colors';
+// import { P2 } from '@/styles/text';
+
+// export const SelectContainer = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   position: relative;
+//   padding: 12px;
+//   border: 1px solid ${COLORS.lightgray};
+//   border-radius: 5px;
+//   cursor: pointer;
+//   height: 44px;
+//   box-sizing: border-box;
+//   width: 100%;
+// `;
+
+// export const DropdownIcon = styled.button`
+//   background: none;
+//   border: none;
+//   cursor: pointer;
+//   display: flex;
+//   align-items: center;
+//   svg {
+//     width: 1.25rem;
+//     height: 1.25rem;
+//   }
+// `;
+
+// export const OptionsContainer = styled.div`
+//   position: absolute;
+//   top: 100%;
+//   left: 0;
+//   right: 0;
+//   background: white;
+//   border: 1px solid ${COLORS.lightgray};
+//   border-radius: 5px;
+//   box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+//   margin-top: 5px;
+//   z-index: 10;
+//   width: 100%;
+// `;
+
+// export const Option = styled(P2).attrs({ as: 'div' })`
+//   padding: 12px;
+//   cursor: pointer;
+//   /* background: ${COLORS.glimpse}; */
+//   color: ${COLORS.shrub};
+//   &:hover {
+//     background: ${COLORS.sproutLight};
+//   }
+// `;
+
 import styled from 'styled-components';
 import COLORS from '@/styles/colors';
 import { P2 } from '@/styles/text';
@@ -14,6 +68,28 @@ export const SelectContainer = styled.div`
   height: 44px;
   box-sizing: border-box;
   width: 100%;
+`;
+
+export const NoBorderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  width: 100%;
+  cursor: pointer;
+`;
+
+export const NoBorderContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+`;
+
+export const HorizontalLine = styled.div`
+  width: 100%;
+  height: 1px;
+  background: ${COLORS.lightgray};
+  margin-top: 4px;
 `;
 
 export const DropdownIcon = styled.button`
@@ -45,7 +121,6 @@ export const OptionsContainer = styled.div`
 export const Option = styled(P2).attrs({ as: 'div' })`
   padding: 12px;
   cursor: pointer;
-  /* background: ${COLORS.glimpse}; */
   color: ${COLORS.shrub};
   &:hover {
     background: ${COLORS.sproutLight};
