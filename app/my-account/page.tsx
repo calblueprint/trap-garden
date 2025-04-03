1aeee6a828f25bca7378878a249d3d4330e30b34
kaleenchen <kaleenchen@berkeley.edu>
2025-04-03 16:49:35 -0700

    final changes

--- app/my-account/page.tsx
+++ app/my-account/page.tsx
@@ -79,7 +79,6 @@ export default function MyAccount() {
       us_state: selectedLocation!,
       user_type: selectedGardenType!,
       has_plot: selectedPlot!,
-      //TODO: add name here to edit name????
     };
     try {
       await setProfile(editedProfileInfo);
@@ -98,22 +97,6 @@ export default function MyAccount() {
       <div>
         {/* User profile image and name */}
         <ProfilePictureContainer>
-          {/* <Flex
-            $pb="1.5rem"
-            $pl="1.5rem"
-            $maxH="0%"
-            $align="center"
-            $minW="100%"
-          >
-            <BackButton onClick={() => router.back()}>
-              <Icon type="backArrow" />
-            </BackButton>
-          </Flex> */}
-
-          {/* TODO: make this into text, add the pen incon, align to the right (put in flexbox with the arrow) */}
-          {/* <button onClick={handleEditMode} style={{}}>
-            {!inEditMode ? 'Edit' : 'Cancel'}
-          </button> */}
           <Flex
             $pb="1.5rem"
             $pl="1.5rem"
@@ -202,34 +185,25 @@ export default function MyAccount() {
             <P2 $fontWeight={400} $color={COLORS.darkgray}>
               Name
             </P2>
-            {/* TODO: make name editable */}
-            {/* <P2
-              style={{
-                marginBottom: '1.5rem',
-              }}
-              $fontWeight={300}
-              $color={COLORS.darkgray}
-            >
-              Kyrene Tam
-            </P2> */}
+
             {inEditMode ? (
               <input
                 type="text"
                 placeholder="Enter name"
                 defaultValue="Kyrene Tam"
                 style={{
-                  fontSize: '0.8rem', // Match the font size to the non-edit mode text
-                  padding: '0.5rem', // Adjust padding if needed
+                  fontSize: '0.8rem',
+                  padding: '0.5rem',
                   borderRadius: '5px',
                   border: `1px solid ${COLORS.darkgray}`,
-                  width: 'auto', // Let it adjust to the content size
-                  minWidth: '120px', // Minimum width to prevent it from getting too small
-                  maxWidth: '130px', // Set a max width based on expected name length
-                  fontFamily: 'Lexand, sans-serif', // Match the font family
-                  color: COLORS.darkgray, // Match the text color
+                  width: 'auto',
+                  minWidth: '120px',
+                  maxWidth: '130px',
+                  fontFamily: 'Lexand, sans-serif',
+                  color: COLORS.darkgray,
                   textAlign: 'left',
-                  fontWeight: 400, // Align text to the left like the non-edit mode text
-                  marginBottom: '1rem', // Add margin to push the next section down
+                  fontWeight: 400,
+                  marginBottom: '1rem',
                 }}
               />
             ) : (
@@ -422,6 +396,3 @@ export default function MyAccount() {
     </Flex>
   );
 }
-//TODO: when in edit mode,the plot option for false is not showing up,
-//change pencil color, change dropdown icon size to smaller,
-//make name editable text input when in edit mode
