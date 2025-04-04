584cbbf23c9789aaa0c093b5807cefb95731adb5
torinnayak1 <torinnayak@berkeley.edu>
2025-04-03 22:57:18 -0700

    [feat] Create Resource Page and FAQ Dropdowns (#87)
    Co-authored-by: Kyle Ramachandran <156966341+kylezryr@users.noreply.github.com>
    Co-authored-by: Kyle Ramachandran <kyleramachandran@berkeley.edu>

---
+++ components/FAQDropdown/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Flex } from '@/styles/containers';
+import Icon from '../Icon';
+import { Answer, Dropdown, HorizontalLine, Question } from './styles';
+
+interface FAQDropdownProps {
+  question: string;
+  answer: string;
+}
+
+export function FAQDropdown({ question, answer }: FAQDropdownProps) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <Flex
+      $direction="column"
+      $gap="1.5rem"
+      $justify="center"
+      $pl="1.5rem"
+      $pr="1.5rem"
+      $mt="1.5rem"
+    >
+      <HorizontalLine />
+      <Flex $direction="row" $justify="between" $align="center">
+        <Question>{question}</Question>
+        <Dropdown onClick={() => setExpanded(!expanded)}>
+          {!expanded ? (
+            <Icon type="dropdownArrowDown"></Icon>
+          ) : (
+            <Icon type="dropdownArrowUp"></Icon>
+          )}
+        </Dropdown>
+      </Flex>
+      {expanded ? <Answer>{answer}</Answer> : null}
+    </Flex>
+  );
+}
---
+++ components/FAQDropdown/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import COLORS from '@/styles/colors';
+
+export const Question = styled.div`
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+export const Answer = styled.div`
+  font-size: 12px;
+  font-weight: 300;
+`;
+
+export const HorizontalLine = styled.div`
+  margin-top: 4px;
+  width: 100%;
+  height: 0.5px;
+  background-color: ${COLORS.lightgray};
+`;
+
+export const Dropdown = styled.div`
+  &:hover {
+    cursor: pointer;
+  }
+`;
