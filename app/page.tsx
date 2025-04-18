c6138e332ae8e18a768e1f7183eb4e5b270527a0
kevin3656 <kevinsolorio@berkeley.edu>
2025-04-17 22:59:40 -0700

    feat: implemented toast component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import BPLogo from '@/assets/images/bp-logo.png';
+import { showToast } from '@/components/Toast';
 import { Container, Image } from './page.style';
 
 export default function Home() {
@@ -6,6 +9,17 @@ export default function Home() {
     <Container>
       <Image src={BPLogo} alt="Blueprint Logo" />
       <p>Home Page is currently under construction!</p>
+      <button
+        onClick={() =>
+          showToast({
+            message: 'Watering Tomato is completed !',
+            undo: true,
+            undoAction: () => console.log('Undo clicked!'),
+          })
+        }
+      >
+        this is a test
+      </button>
     </Container>
   );
 }
