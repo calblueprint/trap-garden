c53cb633da7fbfd6449dfcda97c1de427861ca6e
Kyle Ramachandran <kyleramachandran@berkeley.edu>
2025-05-07 21:50:24 -0700

    remove test toast button

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,6 @@ export default function Home() {
     <Container>
       <Image src={BPLogo} alt="Blueprint Logo" />
       <p>Home Page is currently under construction!</p>
-      <button
-        onClick={() =>
-          showToast({
-            message: 'Watering Tomato is completed !',
-            undo: true,
-            undoAction: () => console.log('Undo clicked!'),
-          })
-        }
-      >
-        this is a test
-      </button>
     </Container>
   );
 }
